0ab9b2f216efb8786582b298fcaf983b29000129
Tomjand <domagala.tomasz@gmail.com>
2021-07-02 18:53:26 +0200

    js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 console.log("test1")
 
-//class userStore {}
-//class userCheck - emailOK, userNameOk
-//getUSerFromDB
+//class userStore { getEmail, getUserName}
+//class dataInputCheck - emailOK, userNameOk (regexp)
+//getUserFromDB
 //isLogin
 //noLogin
